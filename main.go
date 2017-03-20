2287903a3a1c17cb118996a3163d1665e5629966
Moritz F. Kuntze <martllcraft@gmail.com>
2017-03-20 15:47:45 +0100

    Fibinacci nums

--- main.go
+++ main.go
@@ -2,13 +2,28 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 )
 
-func swap(x string, y string) (string, string) {
-	return y, x
-}
-
 func main() {
-	x, y := swap("hello", "world")
-	fmt.Println(x, y)
+	fmt.Println("Bitte gib ein bis zu welcher Grenze Fibonacci-Zahlen ausgegeben werden sollen:")
+	var input string
+	fmt.Scanln(&input)
+
+	i, err := strconv.Atoi(input)
+	for err != nil {
+		fmt.Println("Gib gefälligst ne Zahl ein!")
+		fmt.Scanln(&input)
+		i, err = strconv.Atoi(input)
+	}
+	x := 1
+	y := 1
+	fmt.Println(x)
+	for y <= i {
+		fmt.Println(y)
+		z := x + y
+		x = y
+		y = z
+	}
+
 }
